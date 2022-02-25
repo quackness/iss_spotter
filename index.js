362d75adbe7549166d986b09a78d3ff01c480fc2
Karolina Redden <dubajkaro@gmail.com>
2022-02-25 01:43:30 +0000

    ip address and geo data

---
+++ index.js
@@ -0,0 +1,17 @@
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('142.167.42.164', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
