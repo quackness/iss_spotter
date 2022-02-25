de9e46fd7dccad0d885be8622c3d524584dbca0f
Karolina Redden <dubajkaro@gmail.com>
2022-02-25 01:55:48 +0000

    api request link fix

--- iss.js
+++ iss.js
@@ -1,11 +1,3 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 
 const fetchMyIP = function(callback) {
@@ -25,23 +17,21 @@ const fetchMyIP = function(callback) {
 
 
 const fetchCoordsByIP = function(ip, callback) { //two arguments: ip (string) and callback
-  request(`https://api.freegeoip.app/json/8.8.8.8?apikey=daeaa860-95c0-11ec-95f3-9f8a79d1dfa9`,
+  request(`https://api.freegeoip.app/json/${ip}?apikey=daeaa860-95c0-11ec-95f3-9f8a79d1dfa9`,
   //request(`https://freegeoip.app/json/invalidIPHere`,
     (error, response, body) => {
       if (error) {
         return callback(error, null);
       }
       if (response.statusCode !== 200) {
-        callback(Error(`Status Code ${response.statusCode} when fetching Coordinates 
+        return callback(Error(`Status Code ${response.statusCode} when fetching Coordinates 
       for IP: ${body}`), null);
-        return;
       }
       const { latitude, longitude } = JSON.parse(body);
       return callback(null, { latitude, longitude });
     }
   );
 };
-
 
  
 module.exports = {
